9a6d93ad8eafb05e663c7eab79dfa029395c1b77
Deepak Kumar <tech.deepak5@gmail.com>
2023-01-08 18:45:48 -0800

    Initial commit

---
+++ src/main/java/com/techvigorous/JavaI18nSpringBootWebApplication.java
@@ -0,0 +1,13 @@
+package com.techvigorous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaI18nSpringBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaI18nSpringBootWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techvigorous/JavaI18nSpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techvigorous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaI18nSpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
